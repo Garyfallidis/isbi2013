af7479398beeb9fff1bdf6c94a8068d9e67eedab
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-01-30 11:17:50 -0500

    NF total_variation 4D  with memmaps

--- total_variation.py
+++ total_variation.py
@@ -1,4 +1,7 @@
 import numpy as np
+from tempfile import mkstemp
+from os.path import join as pjoin
+
 
 def tv_denoise_4d(im, weight=100, eps=2.e-4, n_iter_max=200):
     """
@@ -40,17 +43,19 @@ def tv_denoise_4d(im, weight=100, eps=2.e-4, n_iter_max=200):
     res = tv_denoise_4d(mask, weight=0.1)
 
     """
-    px = np.zeros_like(im)
-    py = np.zeros_like(im)
-    pz = np.zeros_like(im)
-    pk = np.zeros_like(im)
+    px = memmap_zeros(im)
+    py = memmap_zeros(im)
+    pz = memmap_zeros(im)
+    pk = memmap_zeros(im)
 
-    gx = np.zeros_like(im)
-    gy = np.zeros_like(im)
-    gz = np.zeros_like(im)
-    gk = np.zeros_like(im)
+    gx = memmap_zeros(im)
+    gy = memmap_zeros(im)
+    gz = memmap_zeros(im)
+    gk = memmap_zeros(im)
 
-    d = np.zeros_like(im)
+    d = memmap_zeros(im)
+    norm = memmap_zeros(im)
+    
     i = 0
     while i < n_iter_max:
         d = - px - py - pz -pk
@@ -90,91 +95,14 @@ def tv_denoise_4d(im, weight=100, eps=2.e-4, n_iter_max=200):
             else:
                 E_previous = E
         i += 1
+
+    del px, py, pz, pk, gx, gy, gz, gk, d
     return out
 
 
-
-def tv_denoise_3d(im, weight=100, eps=2.e-4, n_iter_max=200):
-    """
-    Perform total-variation denoising on 3-D arrays
-
-    Parameters
-    ----------
-    im: ndarray
-        3-D input data to be denoised
-
-    weight: float, optional
-        denoising weight. The greater ``weight``, the more denoising (at 
-        the expense of fidelity to ``input``) 
-
-    eps: float, optional
-        relative difference of the value of the cost function that determines
-        the stop criterion. The algorithm stops when:
-
-            (E_(n-1) - E_n) < eps * E_0
-
-    n_iter_max: int, optional
-        maximal number of iterations used for the optimization.
-
-    Returns
-    -------
-    out: ndarray
-        denoised array
-
-    Notes
-    -----
-    Rudin, Osher and Fatemi algorithm 
-
-    Examples
-    ---------
-    First build synthetic noisy data
-    >>> x, y, z = np.ogrid[0:40, 0:40, 0:40]
-    >>> mask = (x -22)**2 + (y - 20)**2 + (z - 17)**2 < 8**2
-    >>> mask = mask.astype(np.float)
-    >>> mask += 0.2*np.random.randn(*mask.shape)
-    >>> res = tv_denoise_3d(mask, weight=100)
-    """
-    px = np.zeros_like(im)
-    py = np.zeros_like(im)
-    pz = np.zeros_like(im)
-    gx = np.zeros_like(im)
-    gy = np.zeros_like(im)
-    gz = np.zeros_like(im)
-    d = np.zeros_like(im)
-    i = 0
-    while i < n_iter_max:
-        d = - px - py - pz
-        d[1:] += px[:-1] 
-        d[:, 1:] += py[:, :-1] 
-        d[:, :, 1:] += pz[:, :, :-1] 
-        
-        out = im + d
-        E = (d**2).sum()
-
-        gx[:-1] = np.diff(out, axis=0) 
-        gy[:, :-1] = np.diff(out, axis=1) 
-        gz[:, :, :-1] = np.diff(out, axis=2) 
-        norm = np.sqrt(gx**2 + gy**2 + gz**2)
-        E += weight * norm.sum()
-        norm *= 0.5 / weight
-        norm += 1.
-        px -= 1./6.*gx
-        px /= norm
-        py -= 1./6.*gy
-        py /= norm
-        pz -= 1/6.*gz
-        pz /= norm
-        E /= float(im.size)
-        if i == 0:
-            E_init = E
-            E_previous = E
-        else:
-            if np.abs(E_previous - E) < eps * E_init:
-                break
-            else:
-                E_previous = E
-        i += 1
-    return out
+def memmap_zeros(im):
+    _, filename = mkstemp()    
+    return np.memmap(filename, dtype='float64', mode='w+', shape=im.shape)
 
 
 if __name__ == '__main__':
