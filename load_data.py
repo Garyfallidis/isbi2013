632ce0fd0daf7982e54c4364cad6fe255d732985
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-02-01 11:56:29 -0500

    Updated load_data to return data for all 3 categories of datasets

--- load_data.py
+++ load_data.py
@@ -1,32 +1,38 @@
 import nibabel as nib
 from dipy.io.gradients import read_bvals_bvecs
 from dipy.core.gradients import gradient_table
-from dipy.reconst.gqi import GeneralizedQSamplingModel
-from dipy.data import get_sphere
-from dipy.viz.mayavi.spheres import show_odfs
+
 
 dname = 'data/'
 
-fdsi_raw = dname + 'DWIS_dsi-scheme_SNR-30.nii.gz'
-fdsi_bvals = dname + 'dsi-scheme.bval'
-fdsi_bvecs = dname + 'dsi-scheme.bvec'
 
-bvals, bvecs = read_bvals_bvecs(fdsi_bvals, fdsi_bvecs)
-gtab = gradient_table(bvals, bvecs)
-img = nib.load(fdsi_raw)
+def read_data(fimg, fbvals, fbvecs):
+    bvals, bvecs = read_bvals_bvecs(fbvals, fbvecs)
+    gtab = gradient_table(bvals, bvecs)
+    img = nib.load(fimg)
+    data = img.get_data()
+    affine = img.get_affine()
+    return data, affine, gtab
 
-data = img.get_data()
 
-gqi_model = GeneralizedQSamplingModel(gtab,
-                                      method='gqi2',
-                                      sampling_length=2,
-                                      normalize_peaks=False)
+def get_train_dti(snr=30):
+    fimg = dname + 'DWIS_dsi-scheme_SNR-' + str(snr) + '.nii.gz'
+    fbvals = dname + 'dsi-scheme.bval'
+    fbvecs = dname + 'dsi-scheme.bvec'
+    return read_data(fimg, fbvals, fbvecs)
 
-gqi_fit = gqi_model.fit(data)
 
-sphere = get_sphere('symmetric724')
+def get_train_hardi(snr=30):
+    fimg = dname + 'DWIS_hardi-scheme_SNR-' + str(snr) + '.nii.gz'
+    fbvals = dname + 'hardi-scheme.bval'
+    fbvecs = dname + 'hardi-scheme.bvec'
+    return read_data(fimg, fbvals, fbvecs)
 
-gqi_odf = gqi_fit.odf(sphere)
 
-#show_odfs(gqi_odf[25-7:25+7, 25-7:25+7, 21, None], sphere)
-show_odfs(gqi_odf[25-5:25+5, 25-5:25+5, 25][:, None], sphere)
+def get_train_dsi(snr=30):
+    fimg = dname + 'DWIS_dsi-scheme_SNR-' + str(snr) + '.nii.gz'
+    fbvals = dname + 'dsi-scheme.bval'
+    fbvecs = dname + 'dsi-scheme.bvec'
+    return read_data(fimg, fbvals, fbvecs)
+
+
