3b609909db30fb212905c6dc9bf380adcf76dfbd
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-02-27 15:43:24 -0500

    Small example function to save odfs

--- play_gqi.py
+++ play_gqi.py
@@ -9,7 +9,7 @@ from load_data import get_train_dsi
 data, affine, gtab = get_train_dsi(30)
 
 data = data[25 - 10:25 + 10, 25 - 10:25 + 10, 25]
-#data = data[:, :, 25]
+# data = data[:, :, 25]
 
 gqi_model = GeneralizedQSamplingModel(gtab,
                                       method='gqi2',
@@ -21,7 +21,7 @@ sphere = get_sphere('symmetric724')
 gqi_odf = gqi_fit.odf(sphere)
 
 from dipy.viz import fvtk
-r=fvtk.ren()
+r = fvtk.ren()
 fvtk.add(r, fvtk.sphere_funcs(gqi_odf, sphere))
 fvtk.show(r)
 
@@ -39,24 +39,21 @@ def bvl_min_max(b_vector, sphere, sampling_length):
     bv.min()
     bvl = bv * sampling_length / np.pi
     return bvl
-    
+
+
 def H(x):
     return (2 * x * np.cos(x) + (x ** 2 - 2) * np.sin(x)) / x ** 3
 
-sampling_length=1.2
-bvl = bvl_min_max(gqi_model.b_vector, sphere, sampling_length) 
+sampling_length = 1.2
+bvl = bvl_min_max(gqi_model.b_vector, sphere, sampling_length)
 x = np.linspace(bvl.min(), bvl.max(), 100)
 figure(1)
 plot(x, np.sinc(x), 'b', x, H(x), 'g')
 title(str(sampling_length))
 
-sampling_length=3
-bvl = bvl_min_max(gqi_model.b_vector, sphere, sampling_length) 
+sampling_length = 3
+bvl = bvl_min_max(gqi_model.b_vector, sphere, sampling_length)
 x = np.linspace(bvl.min(), bvl.max(), 100)
 figure(2)
 plot(x, np.sinc(x), 'b+', x, H(x), 'g+')
 title(str(sampling_length))
-
-
-
-
