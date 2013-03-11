373d91e430e09bad6043239db3e9663d21affff6
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-03-11 16:50:49 -0400

    tried a few ideas with gqi2 still nothing interesting

--- play_gqi.py
+++ play_gqi.py
@@ -4,34 +4,65 @@ from dipy.reconst.dsi import DiffusionSpectrumDeconvModel
 from dipy.data import get_sphere
 from dipy.viz.mayavi.spheres import show_odfs
 from load_data import get_train_dsi
+from dipy.sims.voxel import MultiTensor
 
 
 data, affine, gtab = get_train_dsi(30)
+# data = data[25 - 10:25 + 10, 25 - 10:25 + 10, 25]
+# data = data[:, :, 25]
 
-#data = data[25 - 10:25 + 10, 25 - 10:25 + 10, 25]
-data = data[:, :, 25]
-
-gqi_model = GeneralizedQSamplingModel(gtab,
-                                      method='gqi2',
-                                      sampling_length=3,
-                                      normalize_peaks=False)
-
-
-from dipy.sims.voxel import MultiTensor
 
 mevals = np.array([[0.0017, 0.0003, 0.0003],
                    [0.0017, 0.0003, 0.0003],
                    [0.0017, 0.0003, 0.0003]])
 
-ang = [(20, 10), (70, 20), (45, 60)]
+# ang = [(20, 10), (70, 20), (45, 60)]
+ang = [(0, 0), (45, 45), (90, 90)]
 
 data, sticks = MultiTensor(gtab, mevals, S0=100, angles=ang,
                            fractions=[33.3, 33.3, 33.4], snr=100)
 
 
+gqi_model = GeneralizedQSamplingModel(gtab,
+                                      method='gqi2',
+                                      sampling_length=4.,
+                                      normalize_peaks=False)
+
 gqi_fit = gqi_model.fit(data)
+
 sphere = get_sphere('symmetric724')
 gqi_odf = gqi_fit.odf(sphere)
+
+
+def optimal_transform(model, data, sphere):
+    from dipy.reconst.gqi import squared_radial_component
+
+    H = squared_radial_component
+
+    b_vector = model.b_vector
+    bnorm = np.sqrt(np.sum(b_vector ** 2, axis=1))
+
+    # # linearize
+    # b_vector2 = b_vector * bnorm[:, None]
+    # b_vector2 /= b_vector.max()
+
+    # # push higher q-space values
+    # bnorm2 = np.sqrt(np.sum(b_vector2 ** 2, axis=1))
+    # bnorm2n = np.interp(bnorm2, [bnorm2.min(), bnorm2.max()], [0, 1])
+    # bpush = bnorm2n ** 4
+
+    # b_vector3 = b_vector * bpush[:, None]
+
+    proj = np.dot(b_vector, sphere.vertices.T)
+    #proj[np.abs(proj) < 0.1] = 0
+    #proj = proj * (bnorm[:, None] / 5.)
+
+
+    gqi_vector = np.real(H(proj * model.Lambda / np.pi))
+
+    return np.dot(data, gqi_vector), proj
+
+odf, proj = optimal_transform(gqi_model, data, sphere)
 
 
 from dipy.viz import fvtk
@@ -46,6 +77,11 @@ dsi_odf = dsi_model.fit(data).odf(sphere)
 fvtk.clear(r)
 fvtk.add(r, fvtk.sphere_funcs(dsi_odf, sphere))
 fvtk.show(r)
+
+fvtk.clear(r)
+fvtk.add(r, fvtk.sphere_funcs(odf, sphere))
+fvtk.show(r)
+
 
 def investigate_internals():
 
