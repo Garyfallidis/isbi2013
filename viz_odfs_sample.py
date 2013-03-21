dce171a2cdf81aac4e93f913d819f92ba008fc1c
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-03-21 14:21:13 -0400

    Final SDT version

--- viz_odfs_sample.py
+++ viz_odfs_sample.py
@@ -13,7 +13,9 @@ def show_odf_sample(filename):
     r = fvtk.ren()
     #odf = odf[:, :, 25]
     #fvtk.add(r, fvtk.sphere_funcs(odf[:, :, None], sphere, norm=True))
-    odf = odf[:, 22, :]
+    #odf = odf[:, 22, :]
+    #odf = odf[14: 23, 22, 34: 43]
+    odf = odf[14:24, 22, 23:33]
     fvtk.add(r, fvtk.sphere_funcs(odf[:, None, :], sphere, norm=True))
 
     fvtk.show(r)
