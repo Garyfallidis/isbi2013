88793351412a8e4d0247ddf4b3dd56e44e808c54
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-10-08 09:59:21 -0400

    added multi-threading fODFs

--- experiment_deconv.py
+++ experiment_deconv.py
@@ -10,7 +10,8 @@ from dipy.data import get_sphere
 #from dipy.viz.mayavi.spheres import show_odfs
 from dipy.reconst.shm import sf_to_sh
 
-from load_data import get_train_dsi, get_train_rois, get_train_mask, get_train_hardi, get_train_dti
+from load_data import (get_train_dsi, get_train_rois, get_train_mask, 
+                       get_train_hardi, get_train_dti, get_test_hardi)
 from show_streamlines import show_streamlines
 from conn_mat import connectivity_matrix
 
@@ -20,7 +21,7 @@ from time import time
 
 
 if __name__ == '__main__':
-    data, affine, gtab = get_train_hardi(30, denoised=1)
+    data, affine, gtab = get_test_hardi(30, denoised=1)
     mask, affine = get_train_mask()
 
     tenmodel = TensorModel(gtab)
@@ -54,22 +55,66 @@ if __name__ == '__main__':
     from dipy.data import get_sphere
     sphere = get_sphere('symmetric724')
 
-    csd_model = ConstrainedSphericalDeconvModel(gtab, (evals, S0))
-    csd_fit = csd_model.fit(data[25 - 10:25 + 10, 25 - 10:25 + 10, 25])
-    csd_odf = csd_fit.odf(sphere)
+    # BEFORE multi-threading
 
-    from dipy.viz import fvtk
-    r = fvtk.ren()
-    fvtk.add(r, fvtk.sphere_funcs(csd_odf, sphere))
-    fvtk.show(r)
+    # csd_model = ConstrainedSphericalDeconvModel(gtab, (evals, S0))
+    # csd_fit = csd_model.fit(data[25 - 10:25 + 10, 25 - 10:25 + 10, 25])
+    # csd_odf = csd_fit.odf(sphere)
+
+    # from dipy.viz import fvtk
+    # r = fvtk.ren()
+    # fvtk.add(r, fvtk.sphere_funcs(csd_odf, sphere))
+    # fvtk.show(r)
     
 
-    csdt_model = ConstrainedSDTModel(gtab, ratio)
-    csdt_fit = csdt_model.fit(data[25 - 10:25 + 10, 25 - 10:25 + 10, 25])
-    csdt_odf = csdt_fit.odf(sphere)
+    # csdt_model = ConstrainedSDTModel(gtab, ratio)
+    # csdt_fit = csdt_model.fit(data[25 - 10:25 + 10, 25 - 10:25 + 10, 25])
+    # csdt_odf = csdt_fit.odf(sphere)
 
+    # from dipy.viz import fvtk
+    # r = fvtk.ren()
+    # fvtk.clear(r)
+    # fvtk.add(r, fvtk.sphere_funcs(csdt_odf, sphere))
+    # fvtk.show(r)
+
+
+    from dipy.reconst.odf import peaks_from_model
+    peaks = peaks_from_model(model=csd_model,
+                             data=data[25 - 10:25 + 10, 25 - 10:25 + 10, 25],
+                             sphere=sphere,
+                             relative_peak_threshold=0.8,
+                             min_separation_angle=45,
+                             return_odf=True, 
+                             return_sh=False, 
+                             normalize_peaks=False,
+                             sh_order=8,
+                             npeaks=5, 
+                             parallel=True, nbr_process=8)
+
+    
+    
     from dipy.viz import fvtk
     r = fvtk.ren()
-    fvtk.clear(r)
-    fvtk.add(r, fvtk.sphere_funcs(csdt_odf, sphere))
+    fvtk.add(r, fvtk.sphere_funcs(peaks.odf, sphere))
     fvtk.show(r)
+
+    from dipy.reconst.odf import peaks_from_model
+    peaks = peaks_from_model(model=csdt_model,
+                             data=data[25 - 10:25 + 10, 25 - 10:25 + 10, 25],
+                             sphere=sphere,
+                             relative_peak_threshold=0.8,
+                             min_separation_angle=45,
+                             return_odf=True, 
+                             return_sh=False, 
+                             normalize_peaks=False,
+                             sh_order=8,
+                             npeaks=5, 
+                             parallel=True, nbr_process=8)
+
+    
+    
+    from dipy.viz import fvtk
+    r = fvtk.ren()
+    fvtk.add(r, fvtk.sphere_funcs(peaks.odf, sphere))
+    fvtk.show(r)
+
